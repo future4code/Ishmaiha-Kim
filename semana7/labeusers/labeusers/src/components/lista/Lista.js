19c0a83430407e0d1391e9c13a70d509fdbc147f
Ishmaiha <ishmaihahkim@gmail.com>
2021-06-17 08:12:52 -0300

    so-consegui-fazer-copiando

--- semana7/labeusers/labeusers/src/components/lista/Lista.js
+++ semana7/labeusers/labeusers/src/components/lista/Lista.js
@@ -1,7 +1,64 @@
 import React from "react"
+import axios from "axios"
+import styled from "styled-components"
+
+const CardUsuario = styled.div`
+    border: 1px solid red;
+    padding: 10px;
+`
 
 export default class Lista extends React.Component {
+    state ={
+        usuarios: [],
+
+    }
+
+
+    componentDidMount () {
+        this.pegarUsuarios()
+    }
+    
+    
+    pegarUsuarios = () =>{
+        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+        axios.get(url,{
+            headers: {
+                Authorization: "ishmaiha-kim-molina"
+            }
+        })
+        .then ((res) => {
+            this.setState({usuarios: res.data})
+        })
+        .catch ((err) => {
+            alert("Erro")
+        })
+    }
+
+    deletarUsuario = (id) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+        axios.delete(url, {
+            headers: {
+                Authorization: "ishmaiha-kim-molina"
+            }
+        })
+        .then ((res) => {
+
+        })
+        .catch ((err) => {
+
+        })
+    }
+    
     render(){
+        const listaUsuarios = this.state.usuarios.map((user) => {
+            return (
+             <CardUsuario key={user.id}>{user.name}
+             <button onClick={() => this.deletarUsuario(user.id)} >
+                 X
+             </button>
+             </CardUsuario>
+            )
+        })
         return (
             <div>
                 <h2>
