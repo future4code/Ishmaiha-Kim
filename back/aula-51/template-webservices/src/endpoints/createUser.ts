c3a41920a379bdf9f5a5e68a6b80c82655f94ca6
Ishmaiha <ishmaihahkim@gmail.com>
2021-08-25 22:11:58 -0300

    exercicios 1 e 2 finalizados

---
+++ back/aula-51/template-webservices/src/endpoints/createUser.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+import { getAddressInfo } from "../services/getAddressInfo";
+import { user } from "../types";
+
+
+export default async function createUser(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+
+      const { name, nickname, email, address, cep, complement, number } = req.body
+
+      getAddressInfo(cep)
+
+      if (!name || !nickname || !email || !address) {
+         res.statusCode = 422
+         throw "'name', 'nickname', 'email' e 'address' são obrigatórios"
+      }
+
+      const id: string = Date.now().toString()
+
+      const newUser: user = { id, name, nickname, email, address }
+
+      await connection('aula51_users').insert(newUser)
+
+      res.status(201).send("Usuário criado!")
+
+   } catch (error) {
+
+      if (typeof error === "string") {
+
+         res.send(error)
+      } else {
+         
+         console.log(error.sqlMessage || error.message);
+         res.status(500).send("Ops! Um erro inesperado ocorreu =/")
+      }
+
+   }
+}
